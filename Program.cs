2d03aa587ea63fd59ab8417f8de774feb3ffce43
Thais Martins <thaismtinocotm@gmail.com>
2022-04-14 20:01:33 -0300

    Multiplicação

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿
-Divisao();
+Multiplicacao();
 
 static void Soma()
 {
@@ -50,4 +50,19 @@ static void Divisao()
     Console.ReadKey();
 }
 
+static void Multiplicacao()
+{
+    Console.Clear();
+
+    Console.WriteLine("Primeiro valor:");
+    float v1 = float.Parse(Console.ReadLine());
+
+    Console.WriteLine("Segundo valor:");
+    float v2 = float.Parse(Console.ReadLine());
+
+    Console.WriteLine("");
+    float resultado = v1 * v2;
+    Console.WriteLine($"O resultado da multiplicação é {resultado}");
+    Console.ReadKey();
+}
 
