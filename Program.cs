e6d6d26cdb5bc6bdcbb690a46db4ba6d308abb83
Thais Martins <thaismtinocotm@gmail.com>
2022-04-14 21:07:31 -0300

    descrição breve do commit

--- Program.cs
+++ Program.cs
@@ -1,5 +1,34 @@
 ﻿
-Multiplicacao();
+Menu();
+
+
+static void Menu()
+{
+    Console.Clear();
+
+    Console.WriteLine("Qual conta deseja fazer?");
+    Console.WriteLine("1 - Soma");
+    Console.WriteLine("2 - Subtração");
+    Console.WriteLine("3 - Divisão");
+    Console.WriteLine("4 - Multiplicação");
+    Console.WriteLine("5 - Sair ");
+
+    Console.WriteLine("----------------");
+    Console.WriteLine("Selecione uma opção: ");
+    short res = short.Parse(Console.ReadLine());
+
+    switch (res) {
+        case 1: Soma(); break;
+        case 2: Subtracao(); break;
+        case 3: Divisao(); break;
+        case 4: Multiplicacao(); break; 
+        case 5: System.Environment.Exit(0); break;
+        default: Menu(); break; 
+    }
+    
+     
+
+}
 
 static void Soma()
 {
@@ -15,6 +44,8 @@ static void Soma()
     float resultado = v1 + v2;
 
     Console.WriteLine("O resultado da soma é: " + resultado);
+    Console.ReadKey();
+    Menu();
 
 }
 
@@ -32,6 +63,7 @@ static void Subtracao()
     float resultado = v1 - v2;
     Console.WriteLine($"O resultado da subtração é {resultado}");
     Console.ReadKey();
+    Menu();
 }
 
 static void Divisao()
@@ -48,6 +80,7 @@ static void Divisao()
     float resultado = v1 / v2;
     Console.WriteLine($"O resultado da divisão é {resultado}");
     Console.ReadKey();
+    Menu();
 }
 
 static void Multiplicacao()
@@ -64,5 +97,6 @@ static void Multiplicacao()
     float resultado = v1 * v2;
     Console.WriteLine($"O resultado da multiplicação é {resultado}");
     Console.ReadKey();
+    Menu();
 }
 
